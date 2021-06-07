22f208f3814592ee60a788fa6f0844c615097cf9
Amit Rawat <amitrawatamit8@gmail.com>
2021-06-07 20:22:33 +0530

    Delete musicplayerdemo directory

--- musicplayerdemo/android/app/src/main/java/com/example/musicplayerdemo/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.musicplayerdemo;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- musicplayerdemo/lib/src/constant/rest_path.dart
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Amit Rawat on 05-06-2021
- */
-
-class RestPath {
-  static const String SEARCH = "search";
-}
